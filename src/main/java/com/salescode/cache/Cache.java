5305104a06bf30f9e2c0760af05f06f1d04d139f
nikhilagarwal07 <nikhil@applicatetechnology.com>
2024-08-05 15:30:25 +0530

    initial commit

---
+++ src/main/java/com/salescode/cache/Cache.java
@@ -0,0 +1,12 @@
+package com.salescode.cache;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Cache {
+
+    public void print(){
+        System.out.println("Hello world from package");
+    }
+
+}
